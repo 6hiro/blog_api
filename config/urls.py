5e07c46ad3148fa4008aad99952aeb0392adbe90
6hiro <hiro6github@gmail.com>
2021-11-24 02:56:05 +0900

    Initial Commit

--- config/urls.py
+++ config/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
     path('blog-admin/', admin.site.urls),
     path('api/v1/', include('apiv1.urls')),
 ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
